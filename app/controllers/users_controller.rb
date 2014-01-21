4f740211972b0640b238ff9fa1fb4e2af2dc5cdc
huy7070 <nguy1702@umn.edu>
2014-01-20 20:21:25 -0600

    Full Working Email Storage Database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new;
   end
+  
+   def create
+    @user = User.new(user_params)    # Not the final implementation!
+    if @user.save
+      redirect_to root_url, notice: "Success! Stay tune!"
+      
+    else
+      render action: 'new', alert: "Signup failed."
+    end
+  end
+  
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:email)
+    end
 end
+
