dfc0c3d5506e10099986f4726e0815a9db2cd9ce
huy7070 <nguy1702@umn.edu>
2014-01-31 17:53:00 -0600

    Added email ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,15 @@ MarblesLandingpage::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+ enable_starttls_auto: true, 
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'myApp.com',
+  user_name:            'testerov3r9000@gmail.com',
+  password:             'Libraries2012',
+  authentication:       'plain'
+   }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140121013313) do
+ActiveRecord::Schema.define(version: 20140127035621) do
+
+  create_table "landings", force: true do |t|
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
