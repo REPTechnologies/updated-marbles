d33e9b17b1311c1c64955d883c70d345442f0684
huy7070 <nguy1702@umn.edu>
2014-02-08 11:24:12 -0600

    Moved Video Down

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140127035621) do
-
-  create_table "landings", force: true do |t|
-    t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140121013313) do
 
   create_table "users", force: true do |t|
     t.string   "email"
