db05349589ac9842ed8fbef5f088e5ea91969d4b
Adam Moen <adam@reptech.io.tld>
2014-12-30 22:19:18 -0600

    add arrow

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -62,4 +62,11 @@ $(document).ready(function(){
 
 	});
 
+	$('#col-container').waypoint(function() {
+  $(".col-3").css({ 
+        opacity: "1",
+        marginTop: "0"
+      });
+}, { offset: 150 });
+	
 });
