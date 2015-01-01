6878bb2619a7b91a8cd0bc7e9f5dae8785aaf094
Adam Moen <adam@reptech.io.tld>
2014-12-31 20:21:11 -0600

    update bottom arrow

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -62,11 +62,13 @@ $(document).ready(function(){
 
 	});
 
-	$('#col-container').waypoint(function() {
+
+});
+	
+$('#col-container').waypoint(function() {
   $(".col-3").css({ 
         opacity: "1",
         marginTop: "0"
       });
 }, { offset: 150 });
 	
-});
